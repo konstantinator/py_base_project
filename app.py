036408e6b4437327c3b69952a600dfe8a424b9ac
konstantin <konstantin7500@yandex.ru>
2023-11-04 14:50:14 +0300

    add readme

--- app.py
+++ app.py
@@ -1,28 +1,46 @@
 import streamlit as st
 import joblib
 from scipy.sparse import hstack
+from scipy.sparse import csr_matrix
 from catboost import CatBoostRegressor
+import re
+from pymorphy2 import MorphAnalyzer
+
+
+morph = MorphAnalyzer()
+patterns = r'[^a-zA-Zа-яА-Я0-9ёЁ]'
+reg = CatBoostRegressor()     
+reg.load_model('./models/model.cbm')
+tf_full = joblib.load('./models/vectorizer_full.pkl')
+
+
+def lemmatize(doc):
+    doc = re.sub(patterns, ' ', doc)
+    tokens = []
+    for token in doc.split():
+        if token:
+            token = token.strip()
+            token = morph.normal_forms(token)[0]
+            tokens.append(token)
+    if len(tokens) > 2:
+        return tokens
+    return []
 
 
 def process():
-    reg = CatBoostRegressor()      # parameters not required.
-    reg.load_model('model.cbm')
-    tf_full = joblib.load('vectorizer_full.pkl')
-
-    # Название
+    # Ввод текста
     st.title("Оценка сложности текстов на основе данных статей N+1")
-
-    # Заголовок
-    st.header("Введите заголовок")
-
     title = st.text_input("Заголовок")
     body = st.text_area("Введите основной текст", height=200)
 
-    # if title and body:
     full_vec = tf_full.transform([title + ' ' + body])
 
     if st.button('Нажми меня'):
-        st.header(f"Оценка сложности текста {round(reg.predict(full_vec)[0], 2)}")
+        len_body = csr_matrix(len(body))
+        full_vec = hstack((tf_full.transform([' '.join(lemmatize(title)) + ' ' + ' '.join(lemmatize(body)) ]), 
+                            len_body
+        ))
+        st.header(f"Оценка сложности текста - {round(reg.predict(full_vec)[0], 2)}")
         
 
 if __name__=='__main__':
